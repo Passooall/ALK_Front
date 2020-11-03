0fb369590cd77d59464680dc7fde70dae23872f4
Passooall <38846013+Passooall@users.noreply.github.com>
2020-11-02 19:46:29 -0800

    Update LoginPage.js
    Created a prototype Login Page for the AL-K

--- ALKapp/Screens/LoginPage.js
+++ ALKapp/Screens/LoginPage.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is a test to see if I can create an app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class LoginPage extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Text>
+            AL-K
+          </Text>
+        </View>
+        <View style = {styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style = {styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            secureTextEntry={true}
+            placeholder="Password.."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})} />
+        </View>
+          <TouchableOpacity>
+            <Text style={styles.forgot}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginBtn}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+           <TouchableOpacity>
+            <Text style={styles.loginText}>Signup</Text>
+          </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#d4d4d4",
+    borderRadius:25,
+    borderColor:"black",
+    height:50,
+    marginBottom:10,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"black",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
   },
 });
